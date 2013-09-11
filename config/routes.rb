903e15a2628228d8cc5a8661652dc027134a55a6
Nicholas Wrenn <nicholas.wrenn@gmail.com>
2013-09-11 14:30:08 -0700

    Removed default index, changed routing to point to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SnapSplat::Application.routes.draw do
+  root to: 'sessions#new'
+  
   resource :session, :only => [:create, :destroy, :new]
   resources :users, :only => [:create, :new, :show, :index] do
     resource :home, :only => [:show]
     resources :photos
     resources :follows, :only => [:create, :destroy]
   end
+  
 end
