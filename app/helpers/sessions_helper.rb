d08271705c142d3616c19562c41ca98c2f9fa940
Nicholas Wrenn <nicholas.wrenn@gmail.com>
2013-09-12 15:55:10 -0700

    Found out I don't know the difference between '>' and '<'

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,8 +14,8 @@ module SessionsHelper
   end
 
   def require_current_user!
-    redirect_to new_session_url if current_user.nil?
-    redirect_to new_session_url if current_user.updated_at > 15.minutes.ago
+      redirect_to new_session_url if current_user.nil?
+      redirect_to new_session_url if current_user.updated_at < 15.minutes.ago
   end
 
   def require_no_current_user!
